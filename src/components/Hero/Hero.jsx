d1e9ea9319439256573ea269c73cad75fa115fec
John Romani <john@anthood.com>
2019-01-06 15:26:45 +0100

    react meets parallax

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import './Hero.css';
-
+import Parallax from 'parallax-js'
 import mainvideo from '../../videos/cover.mp4';
 import bgvideo from '../../videos/bg.mp4';
 import Video from './video';
 
 
 class Hero extends Component {
+
+
+componentDidMount() {
+	const scene = document.getElementById('parascene');
+	new Parallax(scene);
+
+}
 render() {
+	
     return (
+	
       <div id="Hero">
 	  		<video id="bgvideo" autoPlay muted playsInline>
 				<source src={ bgvideo } type="video/mp4"></source>
--- src/components/Hero/video.jsx
+++ src/components/Hero/video.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+
+
 const video = ( props ) => {
+	
 	return (
 		<div className="Video">
-			<video autoPlay loop muted playsInline>
-				<source src={ props.videosrc } type="video/mp4"></source>
-			</video>
-			<h1>Hello there {props.name}</h1>
+			<div id="parascene">
+				<video data-depth="0.7" autoPlay loop muted playsInline>
+					<source src={ props.videosrc } type="video/mp4"></source>
+				</video>
+				<h1 data-depth="0.2">Hello there {props.name}</h1>
+			</div>
 		</div>
 	)
 }
